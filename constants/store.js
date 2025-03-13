c766cdf428fae86f5ab410661fff6571a72fd183
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-13 09:55:00 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "AiG-drone",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "AiGdrone_2025_03_13",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
